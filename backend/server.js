a5cfb0f48caf8a1726fcc8b168d81c53ee0dbd6c
devgurbir <91613146+devgurbir@users.noreply.github.com>
2022-04-01 01:53:59 +0530

    deploy attempt #5

--- backend/server.js
+++ backend/server.js
@@ -66,7 +66,7 @@ app.use("/list", listRouter);
 const start = async () => {
   await connect();
 
-  app.listen(process.env.$PORT, () => {
+  app.listen(process.env.PORT || 3000, () => {
     console.log("listening");
   });
 };
