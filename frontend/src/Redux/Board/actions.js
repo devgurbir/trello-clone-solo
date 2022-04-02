c7a239378b53e731164ef55ff038ab80e61b4aaa
devgurbir <91613146+devgurbir@users.noreply.github.com>
2022-04-02 21:33:10 +0530

    trying to fix bugs in deploy

--- frontend/src/Redux/Board/actions.js
+++ frontend/src/Redux/Board/actions.js
@@ -79,8 +79,7 @@ export const getBoard = (board_id) => async (dispatch) => {
   try {
     dispatch(getBoardRequest());
     const board = await axios.get(
-      // `${process.env.REACT_APP_FRONTEND_ROOT}/board/${board_id}`
-      "https://www.google.com"
+      `${process.env.REACT_APP_FRONTEND_ROOT}/board/${board_id}`
     );
 
     // console.log(board.data);
