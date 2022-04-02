b13c73360da896328eec0f99deac9479476d796c
devgurbir <91613146+devgurbir@users.noreply.github.com>
2022-04-02 13:34:04 +0530

    deleted BoardComponents folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,12 @@
 import { Provider, useDispatch, useSelector } from "react-redux";
-import Board from "./BoardComponent/index";
 import { store } from "./Redux/store";
 import SingleCard from "./Components/Card/SingleCard";
-import AllRoutes from "./Routes/AllRoutes"
+import AllRoutes from "./Routes/AllRoutes";
 
 function App() {
-  
   return (
     <div>
-        <AllRoutes />
+      <AllRoutes />
     </div>
   );
 }
