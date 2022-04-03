e9410b1c30410cc160f895d02b8bad954de16e8c
devgurbir <91613146+devgurbir@users.noreply.github.com>
2022-04-03 06:06:20 +0530

    more changes to signup

--- frontend/src/Redux/User/actions.js
+++ frontend/src/Redux/User/actions.js
@@ -95,7 +95,7 @@ export const loginUser = (email, password) => async (dispatch) => {
     document.cookie = `access_token=Bearer%20${token};max-age=604800;`;
 
     dispatch(loginUserSuccess(user.data.user));
-    window.location.href = `/workspace/${user.workspace[0]}`;
+    window.location.href = `/workspaces/${user.workspace[0]}`;
   } catch (error) {
     dispatch(loginUserFailure(error));
   }
