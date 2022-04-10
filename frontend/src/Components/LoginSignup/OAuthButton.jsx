1e6e7e23690b5d429fb41a248ac98b6b0f6e2520
devgurbir <91613146+devgurbir@users.noreply.github.com>
2022-04-11 01:33:14 +0530

    trying to set cookie on frontend

--- frontend/src/Components/LoginSignup/OAuthButton.jsx
+++ frontend/src/Components/LoginSignup/OAuthButton.jsx
@@ -10,18 +10,13 @@ const OAuthButton = ({ url, label, loginUrl = "#" }) => {
   return (
     <div style={{ marginBottom: "12px" }}>
       {/* <div className={styles.oauthBtn} onClick={() => redirectTo(loginUrl)}> */}
-      <div
-        className={styles.oauthBtn}
-        onClick={() =>
-          axios.get("https://trello-clone-gurbir.herokuapp.com/auth/google", {
-            withCredentials: true,
-          })
-        }
-      >
-        <span className={styles.oauthIcon}>
-          <img src={url} />
-        </span>
-        <span className={styles.oauthLabel}>Continue with {label}</span>
+      <div className={styles.oauthBtn}>
+        <a href="https://trello-clone-gurbir.herokuapp.com/auth/google">
+          <span className={styles.oauthIcon}>
+            <img src={url} />
+          </span>
+          <span className={styles.oauthLabel}>Continue with {label}</span>
+        </a>
       </div>
     </div>
   );
