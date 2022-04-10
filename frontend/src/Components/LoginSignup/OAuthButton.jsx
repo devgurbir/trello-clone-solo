50da60292c8196ea16e079987d7a703236a891cd
devgurbir <91613146+devgurbir@users.noreply.github.com>
2022-04-11 01:18:15 +0530

    trying to set cookie on frontend

--- frontend/src/Components/LoginSignup/OAuthButton.jsx
+++ frontend/src/Components/LoginSignup/OAuthButton.jsx
@@ -3,19 +3,25 @@ import styles from "./loginSignup.module.css";
 import React from "react";
 import redirectTo from "../../Utils/RedirectTo";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const OAuthButton = ({ url, label, loginUrl = "#" }) => {
   console.log(process.env.REACT_APP_BACKEND_ROOT);
   return (
     <div style={{ marginBottom: "12px" }}>
       {/* <div className={styles.oauthBtn} onClick={() => redirectTo(loginUrl)}> */}
-      <div className={styles.oauthBtn}>
-        <Link to="https://trello-clone-gurbir.herokuapp.com/auth/google">
-          <span className={styles.oauthIcon}>
-            <img src={url} />
-          </span>
-          <span className={styles.oauthLabel}>Continue with {label}</span>
-        </Link>
+      <div
+        className={styles.oauthBtn}
+        onClick={() =>
+          axios.get("https://trello-clone-gurbir.herokuapp.com/auth/google", {
+            withCredentials: true,
+          })
+        }
+      >
+        <span className={styles.oauthIcon}>
+          <img src={url} />
+        </span>
+        <span className={styles.oauthLabel}>Continue with {label}</span>
       </div>
     </div>
   );
