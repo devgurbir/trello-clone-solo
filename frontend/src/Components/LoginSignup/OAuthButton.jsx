2653ec3479bf48c1348fd4dca300df1f1508fd2c
devgurbir <91613146+devgurbir@users.noreply.github.com>
2022-04-11 01:00:55 +0530

    trying to set cookie on frontend

--- frontend/src/Components/LoginSignup/OAuthButton.jsx
+++ frontend/src/Components/LoginSignup/OAuthButton.jsx
@@ -2,16 +2,20 @@ import styles from "./loginSignup.module.css";
 
 import React from "react";
 import redirectTo from "../../Utils/RedirectTo";
+import { Link } from "react-router-dom";
 
 const OAuthButton = ({ url, label, loginUrl = "#" }) => {
   console.log(process.env.REACT_APP_BACKEND_ROOT);
   return (
     <div style={{ marginBottom: "12px" }}>
-      <div className={styles.oauthBtn} onClick={() => redirectTo(loginUrl)}>
-        <span className={styles.oauthIcon}>
-          <img src={url} />
-        </span>
-        <span className={styles.oauthLabel}>Continue with {label}</span>
+      {/* <div className={styles.oauthBtn} onClick={() => redirectTo(loginUrl)}> */}
+      <div className={styles.oauthBtn}>
+        <Link to={`${process.env.REACT_APP_BACKEND_ROOT}/auth/google`}>
+          <span className={styles.oauthIcon}>
+            <img src={url} />
+          </span>
+          <span className={styles.oauthLabel}>Continue with {label}</span>
+        </Link>
       </div>
     </div>
   );
