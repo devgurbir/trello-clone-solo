7001df6ab1c496fe9abe69894ab26e90ae003e7e
devgurbir <91613146+devgurbir@users.noreply.github.com>
2022-04-11 03:07:43 +0530

    trying to add cookie on frontend

--- backend/server.js
+++ backend/server.js
@@ -37,15 +37,15 @@ app.use(cookieParser());
 // app.options("*", cors(corsOptions));
 app.use(cors());
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Credentials", true);
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 app.use(express.json());
 
