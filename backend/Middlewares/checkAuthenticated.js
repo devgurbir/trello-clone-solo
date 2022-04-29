30c491794482af4a7ea02809016d0c8ba6d3aad2
devgurbir <91613146+devgurbir@users.noreply.github.com>
2022-04-30 00:33:28 +0530

    made changes to passport, configured express session correctly and got sessions working

--- backend/Middlewares/checkAuthenticated.js
+++ backend/Middlewares/checkAuthenticated.js
@@ -1,23 +1,19 @@
 const {verifyToken} = require("../Utils/token")
 const User = require("../Models/user.model")
 const checkAuthenticated = async (req, res, next) => {
-    const token = req.headers.authorization?.split("Bearer ")[1];
-    if(!token){
-      return res.status(400).send({msg: "Invalid token / token not present"});
+    try {
+      let isAuth = req.isAuthenticated();
+      if(isAuth){
+        next();
+      }
+      else{
+        return res.status(401).send({msg: "You aren't authenticated: ", err});
+      }   
     }
-    console.log(token)
-     
-     try {
-      const payload = await verifyToken(token);
-      const user = await User.findById(payload.id)
-      if(!user) return res.status(401).send({status: "failure", msg: "User does not exist"})
-
-      req.user = user;
-      
-      next()
-  } catch (err) {
+             
+      catch (err) {
       return res.status(500).send({msg: "Something went wrong: ", err});
   }   
-  }
+}
 
   module.exports = checkAuthenticated
